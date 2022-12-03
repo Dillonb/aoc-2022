69cd6e2d45627a6d0a9750583d398d068e6c8907
Dillon Beliveau <dillonbeliveau@gmail.com>
2022-12-02 22:20:04 -0800

    Day 2

---
+++ day02/day02_part1.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+print(sum([{"A X": 1 + 3, "B X": 1 + 0, "C X": 1 + 6, "A Y": 2 + 6, "B Y": 2 + 3, "C Y": 2 + 0, "A Z": 3 + 0, "B Z": 3 + 6, "C Z": 3 + 3}[l.strip()] for l in open("input").readlines()]))
---
+++ day02/day02_part2.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+print(sum([{"A X": 0 + 3, "A Y": 3 + 1, "A Z": 6 + 2, "B X": 0 + 1, "B Y": 3 + 2, "B Z": 6 + 3, "C X": 0 + 2, "C Y": 3 + 3, "C Z": 6 + 1}[l.strip()] for l in open("input").readlines()]))
